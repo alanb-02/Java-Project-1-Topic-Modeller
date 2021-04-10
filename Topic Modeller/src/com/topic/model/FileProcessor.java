5f5b5d9d1f3883330247bbfee435b7dc716a5de0
alanb-02 <mathew02alan@gmail.com>
2021-04-10 01:06:28 +0100

    compare button functionality further developed

--- Topic Modeller/src/com/topic/model/FileProcessor.java
+++ Topic Modeller/src/com/topic/model/FileProcessor.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -66,27 +67,36 @@ public class FileProcessor {
 	
 	public List<String> readFile() throws IOException {
 		//String line = "";
+		openFile();
 		List<String> wordList1 = new ArrayList<String>();
 		List<String> wordList2 = new ArrayList<String>();
 		
 		try {
+			System.out.println(edited_File1.toString());
 			Scanner filescan1 = new Scanner(edited_File1);
 			Scanner filescan2 = new Scanner(edited_File2);
 			
-			while(filescan1.hasNext() || filescan2.hasNext()) {
+			while(filescan1.hasNext()) {
 				String words1 = filescan1.next();
 				words1 = words1.replaceAll("\\p{Punct}", "");
 				words1 = words1.toLowerCase();
 				wordList1.add(words1);
-				
+				System.out.print(words1 + " ");
+			}
+			while(filescan2.hasNext()) {
 				String words2 = filescan2.next();
 				words2 = words2.replaceAll("\\p{Punct}", "");
 				words2 = words2.toLowerCase();
 				wordList2.add(words2);
-				System.out.print(wordList1);
+				System.out.print(words2 + " ");
 			}
 			
-			removeStopWords(wordList1, wordList2);
+			removeStopWords(wordList1);
+			removeStopWords(wordList2);
+			System.out.println();
+			System.out.print(wordList1);
+			System.out.println();
+			System.out.print(wordList2);
 			filescan1.close();
 			filescan2.close();
 		}
@@ -97,24 +107,18 @@ public class FileProcessor {
 		return wordList2;
 	}
 	
-	public List<String> removeStopWords(List<String> arrL1, List<String> arrL2) throws IOException{
-		//File stop_Words = new File("stop-words.txt");
-		List<String> stop_Words_List = Files.readAllLines(Paths.get("stop-words.txt"));
+	public void removeStopWords(List<String> arr) throws IOException{
 		
-		for(String word1 : arrL1) {
-			if(!stop_Words_List.contains(word1)) {
-				arrL1.remove(word1); 
-				return arrL1;
+		List<String> stop_Words_List = Files.readAllLines(Paths.get("C:\\Users\\mathe\\OOP-ca2-TM\\Topic Modeller\\src\\stop-words.txt"));
+		
+		Iterator<String> itr = arr.iterator();
+		while(itr.hasNext()) {
+			String word = itr.next();
+			if(stop_Words_List.contains(word)) {
+				itr.remove(); 
 			}
 		}
-		for(String word2 : arrL2) {
-			if(!stop_Words_List.contains(word2)) {
-				arrL2.remove(word2); // <---------- problem
-				//arrL2.append(" ");
-				return arrL2;
-			}
-		}
-		return arrL1;
+		//return arrL1;
 	}
 	
 }
